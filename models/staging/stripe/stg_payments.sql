e6af7b76b64cd6d089912458ff7966052e4ac77f
Lucas Richard <lrichardlsi2016@gmail.com>
2021-01-30 18:03:31 +0000

    refactor dim_customers to have lifetime_value field totaling payment amounts

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,8 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount
+        amount / 100 as amount,
+        created as created_at
     from analytics.stripe.payment
 )
 
